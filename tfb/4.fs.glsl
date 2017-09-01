be3b7e1ee137b8f30e6c4e4afa39062c92430d66
James Anderson <jrsa@jrsa.co>
2017-08-31 23:27:14 -0700

    particle sicky

---
+++ tfb/4.fs.glsl
@@ -0,0 +1,17 @@
+#version 410 core
+
+in vec2 outVelocity;
+out vec4 outColor;
+
+vec3 hsv2rgb(vec3 color) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(color.xxx + K.xyz) * 6.0 - K.www);
+    vec3 rgb = vec3(color.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), color.y));
+
+    return rgb;
+}
+
+void main()
+{
+    outColor = vec4(hsv2rgb(vec3((outVelocity.x * 0.5) + 0.7, 0.2 + length(outVelocity), 1.0)), 1.0 - length(outVelocity));
+}
---
+++ tfb/4.vs.glsl
@@ -0,0 +1,31 @@
+#version 410 core
+
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec2 velocity;
+layout(location = 2) in vec2 originalPos;
+
+out vec2 outPosition;
+out vec2 outVelocity;
+
+uniform float time;
+uniform vec2 mousePos;
+
+void main()
+{
+    // Points move towards their original position...
+    vec2 newVelocity = originalPos - position;
+
+    // ... unless the mouse is nearby. In that case, they move towards the mouse.
+    // if (length(mousePos - originalPos) < 1.75f) {
+        vec2 acceleration = 1.5f * normalize(mousePos - position);
+        newVelocity = velocity + acceleration * time;
+    // }
+    
+    // if (length(newVelocity) > 1.0f)
+    //     newVelocity = normalize(newVelocity);
+
+    vec2 newPosition = position + newVelocity * time;
+    outPosition = newPosition;
+    outVelocity = newVelocity;
+    gl_Position = vec4(newPosition, 0.0, 1.0);
+}
