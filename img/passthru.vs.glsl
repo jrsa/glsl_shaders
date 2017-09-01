40dc6d0b475bdd5cae7c8f4705c4a9f73385a7b5
James Anderson <jrsa@jrsa.co>
2017-08-31 23:37:03 -0700

    provide pos

--- img/passthru.vs.glsl
+++ img/passthru.vs.glsl
@@ -3,10 +3,12 @@
 layout(location = 0) in vec3 vertexPosition_modelspace;
 layout(location = 1) in vec2 TexCoord;
 
+out vec4 pos;
 
 void main(void)
 {
   gl_Position.xyz = vertexPosition_modelspace;
   gl_Position.w = 1.0;
+  pos = gl_Position;
 }
 
